cce4ce2511b612dbb4bc0baaca63f7721242670f
vshumick <vshumick@gmail.com>
2019-01-22 17:29:23 +0200

    Some changes

--- src/main/java/com/cuckooclock/cucl/CuclApplication.java
+++ src/main/java/com/cuckooclock/cucl/CuclApplication.java
@@ -3,10 +3,15 @@ package com.cuckooclock.cucl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class CuclApplication {
 
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US); //UK
+
         SpringApplication.run(CuclApplication.class, args);
     }
 
